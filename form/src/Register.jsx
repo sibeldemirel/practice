f0c11a026e140bed62bc9195bd6e15791bd4ebee
Sibel Demirel <sibeldmrl19@gmail.com>
2023-02-11 22:13:15 +0100

    register component

--- form/src/Register.jsx
+++ form/src/Register.jsx
@@ -2,7 +2,30 @@ import React, {useState} from 'react';
 
 
 export const Register = () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [name, setName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
+
     return(
-        <>Register</>
+        <>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input value={name} onChange={(e)=>setName(e.target.value)} type="text" name='name' id='name' placeholder='Full Name'/>
+
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+
+            <label htmlFor="password">password</label>
+            <input value={pass} onChange={(e)=>setPass(e.target.value)} type="password" placeholder="*******" id="password" name="password"/>
+
+            <button type="submit">Log In</button>
+        </form>
+        <button>Already have an account ? Login here.</button>
+        </>
     )
 }
